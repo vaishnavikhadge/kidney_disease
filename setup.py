68241c19f2b5b65e7a6fb71317ebb9ebe5f988f2
Vaishnavi khadge <sanikakhadage@gmail.com>
2024-12-14 16:23:32 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools 
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+_version_ = "0.0.0"  
+REPO_NAME = "Kidney_disease"  
+AUTHOR_USER_NAME = "vaishnavikhadge"  
+SRC_REPO = "Kidney_disease"  
+AUTHOR_EMAIL = "sanikakhadage@gmail.com"  
+
+# Configuring the setup
+setuptools.setup(
+    name=SRC_REPO,
+    version=_version_,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Python package for CNN app",  
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",  
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},  
+    packages=setuptools.find_packages(where="src"),  
+)
